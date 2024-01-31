0685e080af5b49e05edaca1ffc2a470d4bd699da
Deepak M R <deepakmreji343@gmail.com>
2024-02-01 00:05:58 +0530

    demo program

--- program codes/demo/sructure.c
+++ program codes/demo/sructure.c
@@ -3,10 +3,12 @@
 /*or
 like this */
 #define pi 3.14
+// define for fixed values such as pi
 int a=1; //global variable outside function
-int main()
+int main()// main a reserved function name(identifier)
 {
     int b;//local variable used only inside the function
-    b=a;
-    printf("%d",b);
+    b=a;//value of a=1 transferred to b, now b=1 too
+    printf("%d",b);//"%d" for decimal type output when run
+    //and b is printed
 }
