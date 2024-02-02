200b521b3ee316242a73db1af8ee6033ad7c8ade
Deepak M R <deepakmreji343@gmail.com>
2024-02-02 21:11:40 +0530

    updated code

--- program codes/friendname/name.c
+++ program codes/friendname/name.c
@@ -3,7 +3,10 @@
 int main()
 {
     char name[25];
+    // name[25] indicates that 25 spaces are there in memory to fill in the name
     printf("Enter friend name:");
+    // & is not neccessary in scan function because data type is string
     scanf("%s",name);
+    // %s to indicste ouput datatype and then variable name
     printf("Hello %s !",name);
 }
