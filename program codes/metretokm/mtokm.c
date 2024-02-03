0f53fda5d159a3c559b019be2a0dff237a02d205
Deepak M R <deepakmreji343@gmail.com>
2024-02-03 23:05:00 +0530

    conversion

---
+++ program codes/metretokm/mtokm.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// program to change km to m
+int main()
+{
+    float km,m;
+    m=km*1000;
+    printf("Enter distance in metre:");
+    scanf("%f",&km);
+    printf("Distance in metre = %fm",m);
+}
