e2f66f0d0ada05ff0a07f3736869d89e2562de03
Deepak M R <deepakmreji343@gmail.com>
2024-02-15 00:42:28 +0530

    Create greaterin3.c
    code

---
+++ program codes/greaterin3/greaterin3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+//to find the greatest of 2 numbers
+int main()
+{
+    int n1,n2,n3;
+    printf("Enter any 3 numbers:\n");
+    scanf("%d",n1);    
+    scanf("%d",n2);    
+    scanf("%d",n3);    
+    if (n1>n2 && n1>n3)
+    {
+       printf("%d is greater than %d and %d",n1,n2,n3);
+    }
+    else if (n1<n2 && n2>n3)
+    {
+       printf("%d is greater than %d and %d",n2,n1,n3);
+    }
+    else if (n3<n2 && n2<n3)
+    {
+       printf("%d is greater than %d and %d",n3,n1,n2);
+    }
+    else
+    {
+       printf("Some numbers are equal");
+    }
+    
+}
