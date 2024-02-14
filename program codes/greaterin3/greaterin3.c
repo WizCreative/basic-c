751a527b9f1a8f978daf65d36e2399e37768c5b2
Deepak M R <deepakmreji343@gmail.com>
2024-02-15 00:46:59 +0530

    Update greaterin3.c
    code

--- program codes/greaterin3/greaterin3.c
+++ program codes/greaterin3/greaterin3.c
@@ -4,9 +4,9 @@ int main()
 {
     int n1,n2,n3;
     printf("Enter any 3 numbers:\n");
-    scanf("%d",n1);    
-    scanf("%d",n2);    
-    scanf("%d",n3);    
+    scanf("%d",&n1);    
+    scanf("%d",&n2);    
+    scanf("%d",&n3);    
     if (n1>n2 && n1>n3)
     {
        printf("%d is greater than %d and %d",n1,n2,n3);
