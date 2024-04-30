c74157ac82cda8b4aab84ba349dcd3e59e512480
Ashwin <ashwinshine.nonu@gmail.com>
2024-04-30 22:38:48 +0530

    Comments and bug fixing in matrix codes

--- program codes/Matrix/transpose.c
+++ program codes/Matrix/transpose.c
@@ -1,30 +1,35 @@
-#include<stdio.h>
-int main()
-{
-    int col,row;
-    printf("Number of columns: ");
-    scanf("%d",&col);
-    printf("Number of rows: ");
-    scanf("%d",&row);
+/*Matrix Transpose
+ * Authors - Deepak M R, Ashwin P Shine
+ *
+ *
+ * */
 
-    int matrix[row][col];
+#include <stdio.h>
+void main() {
+  // This is to get the number of rows and coloumns of the Matrix
+  int col, row;
+  printf("Number of columns: ");
+  scanf("%d", &col);
+  printf("Number of rows: ");
+  scanf("%d", &row);
 
-    for(int i=0;i<row;i++)
-    {
-        for(int j=0;j<col;j++)
-        {
-            printf("Enter the %d row and %d column: ",i+1,j+1);
-            scanf("%d",&matrix[i][j]);
-        }
-        printf("\n");
+  // Intializing the matrix and getting the input
+  int matrix[row][col];
+
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      printf("Enter the %d row and %d column: ", i + 1, j + 1);
+      scanf("%d", &matrix[i][j]);
     }
-    printf("The Transpose is : \n");
-    for(int i=0;i<row;i++)
-    {
-        for(int j=0;j<col;j++)
-        {
-            printf("%d",matrix[i][j]);
-        }
-        printf("\n");
+    printf("\n");
+  }
+
+  // Printing the matrix upside down (switching i and j)
+  printf("The Transpose is : \n");
+  for (int i = 0; i < row; i++) {
+    for (int j = 0; j < col; j++) {
+      printf("%d", matrix[i][j]);
     }
+    printf("\n");
+  }
 }
